aa12bea61f0027c1b612b3d779ee5dc186c62ee5
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2023-12-15 18:15:54 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import { RecipeList } from './RecipeList';
 import recipes from '../recipes.json';
+import { GlobalStyle } from './GlobalStyle';
 
 export const App = () => {
-  return <RecipeList items={recipes} />;
+  return (
+    <div>
+      <RecipeList items={recipes} />
+      <GlobalStyle />
+    </div>
+  );
 };
 
 // https://react-icons.github.io/react-icons/
+// https://styled-components.com/docs/api
